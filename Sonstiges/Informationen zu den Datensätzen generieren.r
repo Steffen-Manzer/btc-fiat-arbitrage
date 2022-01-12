93991596c1404a3829c464356618fc3f10cbfe52
fox34 <github@noecho.de>
2022-01-12 14:52:53 +0100

    [Sonstiges] Überblick über die vorliegenden Datensätze für LaTeX generieren

---
+++ Sonstiges/Informationen zu den Datensätzen generieren.r
@@ -0,0 +1,49 @@
+# TODO Automatisch aus LaTeX heraus aktualisieren?
+library("fst")
+library("stringr")
+source("Funktionen/FormatCurrencyPair.r")
+source("Funktionen/FormatNumber.r")
+source("Funktionen/printf.r")
+
+sources <- c("Bitfinex", "Bitstamp", "Coinbase", "Kraken", "TrueFX", "Dukascopy")
+
+for (source in sources) {
+    pairs <- list.files(sprintf("Cache/%s", tolower(source)))
+    for (pair in pairs) {
+        files <- list.files(
+            sprintf("Cache/%s/%s/tick", source, pair),
+            full.names=TRUE
+        )
+        
+        numTicks <- 0L
+        for (i in seq_along(files)) {
+            f <- files[i]
+            
+            # Anzahl Ticks lesen
+            fileStats <- metadata_fst(f)
+            numTicks <- numTicks + fileStats$nrOfRows
+            
+            # Ersten Datensatz lesen
+            if (i == 1L) {
+                firstDataset <- read_fst(f, columns=c("Time"), to=1L, as.data.table=TRUE)
+                firstDataset <- firstDataset$Time[1L]
+            }
+            
+            # Letzten Datensatz lesen
+            if (i == length(files)) {
+                lastDataset <- read_fst(f, columns=c("Time"), 
+                                        from=fileStats$nrOfRows, to=fileStats$nrOfRows,
+                                        as.data.table=TRUE)
+                lastDataset <- lastDataset$Time[1L]
+            }
+        }
+        
+        source_formatted <- str_replace(source, "Coinbase", "Coinbase Pro")
+        printf("        %s &\n", source_formatted) # Börse / Datenquelle
+        printf("            %s &\n", format.currencyPair(pair)) # Kurspaar
+        printf("            %s &\n", format.number(numTicks)) # Anzahl Ticks
+        printf("            %s &\n", format(firstDataset, "%d.%m.%Y %H:%M:%S")) # Von
+        printf("            %s \\\\\n", format(lastDataset, "%d.%m.%Y %H:%M:%S")) # Bis
+        printf("\n")
+    }
+}
