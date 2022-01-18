64821d70c7d2aeb694047ca952019ae4d4fe6dda
fox34 <github@noecho.de>
2022-01-18 11:08:23 +0100

    [Funktionen] Neue Hilfsfunktion `readTickDataByPeriod`

---
+++ Funktionen/ReadTickDataByPeriod.r
@@ -0,0 +1,102 @@
+# Benötigte Funktionen und Pakete laden
+source("Funktionen/AddOneMonth.r")
+source("Funktionen/ReadDataFileChunked.r")
+library("data.table")
+library("lubridate") # is.POSIXct
+library("rjson")
+
+#' Lade Tickdaten für das gesamte angegebene Intervall, ggf. über
+#' mehrere Quelldateien hinweg
+#' 
+#' @param exchange Die gewünschte Börse / Datenquelle
+#' @param currencyPair Der gewünschte Wechselkurs
+#' @param startTime Zeitpunkt des aktuellen (zuletzt verwendeten) Ticks
+#' @param endTime Zieldatum, bis zu dem mindestens gelesen werden soll
+#' @param filterSuspiciousPeriods Auffällige Datensätze herausfiltern
+#' @param ... Weitere Parameter, die an `readDataFileChunked` übergeben werden
+#' @return `data.table` mit den Originaldaten
+readTickDataByPeriod <- function(
+    exchange,
+    currencyPair,
+    startTime,
+    endTime,
+    filterSuspiciousPeriods = TRUE,
+    ...
+) {
+    
+    # Parameter validieren
+    stopifnot(
+        is.character(exchange), length(exchange) == 1L,
+        is.character(currencyPair), length(currencyPair) == 1L,
+        is.POSIXct(startTime), length(startTime) == 1L,
+        is.POSIXct(endTime), length(endTime) == 1L,
+        is.logical(filterSuspiciousPeriods), length(filterSuspiciousPeriods) == 1L
+    )
+    
+    # Anomalien aus diesem Datensatz filtern
+    if (filterSuspiciousPeriods == TRUE) {
+        allExchangeMetadata <- fromJSON(file="Daten/bitcoin-metadata.json")
+        exchangeMetadata <- allExchangeMetadata[[exchange]]
+        stopifnot(!is.null(exchangeMetadata))
+        pairMetadata <- exchangeMetadata[[currencyPair]]
+        stopifnot(!is.null(pairMetadata))
+        suspiciousPeriods <- data.table()
+        for (i in seq_along(pairMetadata$suspiciousPeriods)) {
+            period <- pairMetadata$suspiciousPeriods[[i]]
+            if (isFALSE(period$filter)) {
+                next
+            }
+            suspiciousPeriods <- rbind(suspiciousPeriods, data.table(
+                startDate = as.POSIXct(period$startDate, tz="UTC"),
+                endDate = as.POSIXct(period$endDate, tz="UTC")
+            ))
+        }
+    }
+    
+    # Beginne bei aktuellem Monat
+    currentTime <- startTime
+    result <- data.table()
+    
+    while (TRUE) {
+        
+        dataFile <- sprintf(
+            "Cache/%s/%s/tick/%1$s-%2$s-tick-%3$d-%4$02d.fst",
+            exchange, tolower(currencyPair), year(currentTime), month(currentTime)
+        )
+        if (!file.exists(dataFile)) {
+            stop(sprintf("Datei nicht gefunden: %s", dataFile))
+        }
+        
+        newData <- readDataFileChunked(dataFile, 1L, endTime, ...)
+        
+        # Auf gewünschten Zeitraum beschränken
+        newData <- newData[Time %between% c(startTime, endTime)]
+        if (nrow(newData) > 0L) {
+            
+            # Filtern
+            if (filterSuspiciousPeriods == TRUE) {
+                for (i in seq_len(nrow(suspiciousPeriods))) {
+                    filtered <- newData[
+                        Time %between% c(suspiciousPeriods$startDate[i], suspiciousPeriods$endDate[i]),
+                        which=TRUE
+                    ]
+                    if (length(filtered) > 0) {
+                        newData <- newData[!filtered]
+                    }
+                }
+            }
+            
+            result <- rbindlist(list(result, newData), use.names=TRUE)
+        }
+        
+        # Ziel-Monat erreicht, Ende.
+        if (as.integer(format(endTime, "%Y%m")) <= as.integer(format(currentTime, "%Y%m"))) {
+            break
+        }
+        
+        # Lese zusätzlich nächsten Monat
+        currentTime <- addOneMonth(currentTime)
+    }
+    
+    return(result)
+}
