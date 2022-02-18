3fd771327364c2b212c05c1038cbdb417dcd45de
fox34 <github@noecho.de>
2022-02-18 13:49:37 +0100

    [Funktionen] `mergeSortAndFilter` in eigene Datei ausgelagert

--- Raumarbitrage/Bitcoin - Abweichungen berechnen.r
+++ Raumarbitrage/Bitcoin - Abweichungen berechnen.r
@@ -34,6 +34,7 @@ source("Funktionen/FormatCurrencyPair.r")
 source("Funktionen/FormatDuration.r")
 source("Funktionen/FormatNumber.r")
 source("Funktionen/FormatPOSIXctWithFractionalSeconds.r")
+source("Funktionen/MergeSortAndFilter.r")
 source("Funktionen/ReadTickDataAsMovingWindow.r")
 source("Funktionen/printf.r")
 library("fst")
@@ -122,61 +123,6 @@ summariseMultipleTicksAtSameTime <- function(dataset) {
         ), 
         by=Time
     ])
-}
-
-
-#' Zwei Datensätze in eine gemeinsame Liste zusammenführen
-#' 
-#' Verbindet zwei Sätze von Tickdaten in eine gemeinsame Liste,
-#' sortiert diese nach Zeit und entfernt die mittleren von
-#' drei oder mehr aufeinanderfolgenden Ticks der selben Börse, 
-#' da diese für die Auswertung nicht relevant sind.
-#' 
-#' @param dataset_a `data.table` mit mindestens den Spalten `Time` und `Exchange`
-#' @param dataset_b Wie `dataset_a`.
-#' @return `data.table` Eine Tabelle der Tickdaten beider Börsen
-mergeSortAndFilterTwoDatasets <- function(dataset_a, dataset_b) {
-    
-    # Merge, Sort und Filter (nicht: mergesort-Algorithmus)
-    
-    # Daten zu einer gemeinsamen Liste verbinden
-    dataset_ab <- rbindlist(list(dataset_a, dataset_b), use.names=TRUE)
-    
-    # Liste nach Zeit sortieren
-    setorder(dataset_ab, Time)
-    
-    # `dataset_ab` enthält nun Ticks beider Börsen nach Zeit sortiert.
-    # Aufeinanderfolgende Daten der selben Börse interessieren nicht, da der Tickpunkt
-    # davor bzw. danach immer näher am nächsten Tick der anderen Börse ist.
-    # Aufeinanderfolgende Tripel daher herausfiltern, dies beschleunigt
-    # die weitere Verarbeitung signifikant (etwa um den Faktor 5).
-    #
-    # Beispiel:
-    # |--------------------------------->   Zeitachse
-    #  A A A A B B A B B B A A B B B B A    Originaldaten (Einzelne Ticks der Börse A oder B)
-    #         *   * *     *   *       *     Sinnvolle Preisvergleiche
-    #  * * *           *         * *        Nicht benötigte Ticks
-    #        A B B A B   B A A B     B A    Reduzierter Datensatz
-    
-    # Tripel filtern
-    # Einschränkung: Erste und letzte Zeile werden nie entfernt,
-    # diese können an dieser Stelle nicht sinnvoll geprüft werden.
-    # Im weiteren Verlauf wird erneut auf die gleiche Börse 
-    # aufeinanderfolgender Ticks geprüft.
-    triplets <- c(
-        FALSE, # Ersten Tick immer beibehalten
-        rollapply(
-            dataset_ab$Exchange,
-            width = 3,
-            # Es handelt sich um ein zu entfernendes Tripel, wenn  die
-            # Börse im vorherigen, aktuellen und nächsten Tick identisch ist
-            FUN = function(exchg) (exchg[1] == exchg[2] && exchg[2] == exchg[3])
-        ),
-        FALSE # Letzten Tick immer beibehalten
-    )
-    
-    # Datensatz ohne gefundene Tripel zurückgeben
-    return(dataset_ab[!triplets])
 }
 
 
