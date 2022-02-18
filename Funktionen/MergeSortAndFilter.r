c326d975e9a24fa9285ad6cb234bbfc8c6794424
fox34 <github@noecho.de>
2022-02-18 18:18:38 +0100

    [Funktionen] Kommentare aus `mergeSortAndFilter` reduziert

--- Funktionen/MergeSortAndFilter.r
+++ Funktionen/MergeSortAndFilter.r
@@ -5,45 +5,26 @@ library("zoo") # rollapply
 #' Zwei Datensätze in eine gemeinsame Liste zusammenführen
 #' 
 #' Verbindet zwei Sätze von Tickdaten in eine gemeinsame Liste,
-#' sortiert diese nach Zeit und entfernt die mittleren von
-#' drei oder mehr aufeinanderfolgenden Ticks der selben Börse, 
-#' da diese für die Auswertung nicht relevant sind.
+#' sortiert diese nach Zeit und entfernt alle mittleren von
+#' drei oder mehr aufeinanderfolgenden Ticks der selben Börse.
 #' 
-#' @param dataset_a `data.table` die mindestens die
-#'                  Spalten `Time` und `Exchange` enthält
+#' @param dataset_a `data.table` mit den Spalten `Time` und `Exchange`
 #' @param dataset_b Wie `dataset_a`.
 #' @return `data.table` Eine Tabelle der Tickdaten beider Börsen
-mergeSortAndFilterTwoDatasets <- function(dataset_a, dataset_b) {
-    
-    # Merge, Sort und Filter (nicht: mergesort-Algorithmus)
-    # Daten zu einer gemeinsamen Liste verbinden
-    dataset_ab <- rbindlist(
-        list(dataset_a, dataset_b),
-        use.names = TRUE
-    )
-    
-    # Liste nach Zeit sortieren
+mergeSortAndFilterTwoDatasets <- function(dataset_a, dataset_b)
+{
+    # Daten zu einer gemeinsamen Liste verbinden, nach Zeit sortieren
+    dataset_ab <- rbindlist(list(dataset_a, dataset_b))
     setorder(dataset_ab, Time)
     
-    # `dataset_ab` enthält nun Ticks beider Börsen nach Zeit sortiert.
-    # Aufeinanderfolgende Daten der selben Börse interessieren nicht,
-    # da der Tickpunkt davor bzw. danach immer näher am nächsten Tick 
-    # der anderen Börse ist. Aufeinanderfolgende Tripel daher
-    # herausfiltern, dies beschleunigt die weitere Verarbeitung
-    # signifikant (etwa um den Faktor 5).
-    #
+    # Tripel heraussuchen. Einschränkung: Erste und letzte Zeile
+    # werden nie entfernt, diese können hier nicht geprüft werden.
     # Beispiel:
     # |--------------------------------->   Zeitachse
     #  A A A A B B A B B B A A B B B B A    Ticks der Börsen A und B
     #         *   * *     *   *       *     Sinnvolle Preisvergleiche
     #  * * *           *         * *        Nicht benötigte Ticks
     #        A B B A B   B A A B     B A    Reduzierter Datensatz
-    
-    # Tripel filtern
-    # Einschränkung: Erste und letzte Zeile werden nie entfernt,
-    # diese können an dieser Stelle nicht sinnvoll geprüft werden.
-    # Im weiteren Verlauf wird erneut auf die gleiche Börse 
-    # aufeinanderfolgender Ticks geprüft.
     triplets <- c(
         FALSE, # Ersten Tick immer beibehalten
         rollapply(
@@ -57,6 +38,6 @@ mergeSortAndFilterTwoDatasets <- function(dataset_a, dataset_b) {
         FALSE # Letzten Tick immer beibehalten
     )
     
-    # Datensatz ohne gefundene Tripel zurückgeben
+    # Datensatz ohne die identifizierten Tripel zurückgeben
     return(dataset_ab[!triplets])
 }
