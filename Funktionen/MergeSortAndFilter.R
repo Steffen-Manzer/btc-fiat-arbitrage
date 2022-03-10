347dd5b16d15737fa39b42ee8e11422ed55f6fdb
fox34 <github@noecho.de>
2022-03-10 12:11:21 +0100

    [Funktionen] `mergeSortAndFilter` kann Tripel nun wahlweise nach Börse oder nach Kurspaar filtern

--- Funktionen/MergeSortAndFilter.R
+++ Funktionen/MergeSortAndFilter.R
@@ -6,34 +6,39 @@ library("zoo") # rollapply
 #' 
 #' Verbindet zwei Sätze von Tickdaten in eine gemeinsame Liste,
 #' sortiert diese nach Zeit und entfernt alle mittleren von
-#' drei oder mehr aufeinanderfolgenden Ticks der selben Börse.
+#' drei oder mehr aufeinanderfolgenden Ticks der selben Börse oder
+#' des selben Wechselkurses (`compare_by`).
 #' 
-#' @param dataset_a `data.table` mit den Spalten `Time` und `Exchange`
+#' Beispiel Raumarbitrage: Vergleich anhand der Börse:
+#' |--------------------------------->   Zeitachse
+#'  A A A A B B A B B B A A B B B B A    Ticks der Börsen A oder B
+#'         *   * *     *   *       *     Sinnvolle Preisvergleiche
+#'  ? * *           *         * *        Nicht benötigte Ticks
+#'  A     A B B A B   B A A B     B A    Reduzierter Datensatz
+#' 
+#' @param dataset_a `data.table` mit `Time` und `compare_by` (s.u.)
 #' @param dataset_b Wie `dataset_a`.
+#' @param compare_by Spalte anhand derer doppelte Ticks erkannt werden
 #' @return `data.table` Eine Tabelle der Tickdaten beider Börsen
-mergeSortAndFilterTwoDatasets <- function(dataset_a, dataset_b)
-{
+mergeSortAndFilterTwoDatasets <- function(
+    dataset_a,
+    dataset_b,
+    compare_by = "Exchange"
+) {
     # Daten zu einer gemeinsamen Liste verbinden, nach Zeit sortieren
     dataset_ab <- rbindlist(list(dataset_a, dataset_b))
     setorder(dataset_ab, Time)
     
-    # Tripel heraussuchen. Einschränkung: Erste und letzte Zeile
-    # werden nie entfernt, diese können hier nicht geprüft werden.
-    # Beispiel:
-    # |--------------------------------->   Zeitachse
-    #  A A A A B B A B B B A A B B B B A    Ticks der Börsen A und B
-    #         *   * *     *   *       *     Sinnvolle Preisvergleiche
-    #  ? * *           *         * *        Nicht benötigte Ticks
-    #  A     A B B A B   B A A B     B A    Reduzierter Datensatz
+    # Tripel heraussuchen
     triplets <- c(
         FALSE, # Ersten Tick immer beibehalten
         rollapply(
-            data = dataset_ab$Exchange,
+            data = dataset_ab[[compare_by]],
             # Es handelt sich um einen zu entfernenden Datenpunkt,
-            # wenn die Börse im vorherigen, aktuellen und nächsten 
-            # Tick identisch ist. Daher Fensterbreite: 3 Ticks.
+            # wenn der Vergleichswert im vorherigen, aktuellen und 
+            # nächsten Tick identisch ist. Daher: Fensterbreite 3
             width = 3,
-            FUN = function(exchg) all(exchg == exchg[1])
+            FUN = function(x) all(x == x[1])
         ),
         FALSE # Letzten Tick immer beibehalten
     )
