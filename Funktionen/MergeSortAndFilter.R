c0f2127048a8b10b6c8b49f24765792033e8844c
fox34 <github@noecho.de>
2022-03-11 11:39:33 +0100

    [Funktionen] `mergeSortAndFilterTwoDatasets`: Nutze Argument `fill` in `rollapply`

--- Funktionen/MergeSortAndFilter.R
+++ Funktionen/MergeSortAndFilter.R
@@ -2,8 +2,6 @@
 library("data.table") # rbindlist, setorder
 library("zoo") # rollapply
 
-#' Zwei Datensätze in eine gemeinsame Liste zusammenführen
-#' 
 #' Verbindet zwei Sätze von Tickdaten in eine gemeinsame Liste,
 #' sortiert diese nach Zeit und entfernt alle mittleren von
 #' drei oder mehr aufeinanderfolgenden Ticks der selben Börse oder
@@ -21,26 +19,22 @@ library("zoo") # rollapply
 #' @param compare_by Spalte anhand derer doppelte Ticks erkannt werden
 #' @return `data.table` Eine Tabelle der Tickdaten beider Börsen
 mergeSortAndFilterTwoDatasets <- function(
-    dataset_a,
-    dataset_b,
-    compare_by = "Exchange"
+    dataset_a, dataset_b, compare_by = "Exchange"
 ) {
     # Daten zu einer gemeinsamen Liste verbinden, nach Zeit sortieren
     dataset_ab <- rbindlist(list(dataset_a, dataset_b))
     setorder(dataset_ab, Time)
     
     # Tripel heraussuchen
-    triplets <- c(
-        FALSE, # Ersten Tick immer beibehalten
-        rollapply(
-            data = dataset_ab[[compare_by]],
-            # Es handelt sich um einen zu entfernenden Datenpunkt,
-            # wenn der Vergleichswert im vorherigen, aktuellen und 
-            # nächsten Tick identisch ist. Daher: Fensterbreite 3
-            width = 3,
-            FUN = function(x) all(x == x[1])
-        ),
-        FALSE # Letzten Tick immer beibehalten
+    triplets <- rollapply(
+        data = dataset_ab[[compare_by]],
+        # Es handelt sich um einen zu entfernenden Datenpunkt,
+        # wenn der Vergleichswert im vorherigen, aktuellen und 
+        # nächsten Tick identisch ist. Daher: Fensterbreite 3
+        width = 3,
+        FUN = function(x) all(x == x[1]),
+        # Mit `FALSE` auffüllen (ersten und letzten Tick nie filtern)
+        fill = FALSE
     )
     
     # Datensatz ohne die identifizierten Tripel zurückgeben
