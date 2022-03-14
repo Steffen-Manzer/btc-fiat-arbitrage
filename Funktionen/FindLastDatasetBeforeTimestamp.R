d17a4bd8c53036395bbe1be5038d4911f5d956a4
fox34 <github@noecho.de>
2022-03-14 10:15:01 +0100

    [Funktionen] `findLastDatasetBeforeTimestamp` korrigiert

--- Funktionen/FindLastDatasetBeforeTimestamp.R
+++ Funktionen/FindLastDatasetBeforeTimestamp.R
@@ -1,18 +1,24 @@
 #' Letzten Datensatz vor einem angegebenen Datum finden
 #' 
-#' @param d Eine `data.table` mit mind. der Spalte `Time`
+#' @param d Eine nach Zeit sortierte `data.table` mit der Spalte `Time`
 #' @param before Ein POSIXct der Länge 1
 #' @return Die Zeile aus `d`, die zeitlich am nächsten vor `before` liegt
 findLastDatasetBeforeTimestamp <- function(d, before)
 {
-    # Angelehnt an "safeWhichMax" aus 
-    # https://stackoverflow.com/a/71052939/3238708
-    last <- which.min(d$Time <= before)
+    # Alle Daten liegen vor dem angegebenen Datum
+    if (last(d$Time) <= before) {
+        return(last(d))
+    }
     
-    # which.min gibt auch dann 1 zurück, wenn kein Datensatz passt
-    if (last == 1L && d$Time[1] > before) {
+    # Alle Daten liegen nach dem angegebenen Datum
+    if (d$Time[1] > before) {
         return(NA)
     }
+    
+    # Angelehnt an https://stackoverflow.com/a/71052939/3238708
+    # Da die Grenzen oben bereits geprüft wurden, gibt which.min hier immer
+    # einen Wert zwischen 2 und n zurück.
+    last <- which.min(d$Time <= before) - 1L
     
     return(d[last])
 }
