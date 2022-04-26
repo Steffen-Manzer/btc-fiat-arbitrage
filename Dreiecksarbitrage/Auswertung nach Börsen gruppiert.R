743107336d5ffbdc1aa0f351b18440f570a5adf4
fox34 <github@noecho.de>
2022-04-26 17:28:18 +0200

    [Dreiecksarbitrage] Auswertung: Überblicksgrafik in erster Fassung

--- Dreiecksarbitrage/Auswertung nach Börsen gruppiert.R
+++ Dreiecksarbitrage/Auswertung nach Börsen gruppiert.R
@@ -27,17 +27,30 @@ library("data.table")
 library("lubridate") # floor_date
 library("ggplot2")
 library("ggthemes")
+library("gridExtra") # grid.arrange
+library("readr") # read_file, write_file
 #library("scales") # breaks_extended
+library("stringr") # str_replace
 library("tictoc")
 
 
 # Konfiguration -----------------------------------------------------------
 plotAsLaTeX <- FALSE
-exchanges <- list(
-    "bitfinex"="Bitfinex",
-    "bitstamp"="Bitstamp",
-    "coinbase"="Coinbase Pro",
-    "kraken"="Kraken"
+exchangeNames <- list(
+    "bitfinex" = "Bitfinex",
+    "bitstamp" = "Bitstamp",
+    "coinbase" = "Coinbase Pro",
+    "kraken" = "Kraken"
+)
+
+# TODO.
+# Die Breakpoints selbst werden immer dem letzten der beiden entstehenden
+# Intervalle zugerechnet
+breakpointsByExchange <- list(
+    "bitfinex" = NULL,
+    "bitstamp" = NULL,
+    "coinbase" = NULL,
+    "kraken" = NULL
 )
 
 #' Tabellen-Template mit `{tableContent}`, `{tableCaption` und `{tableLabel}` 
@@ -184,10 +197,6 @@ aggregateResultsByTime <- function(result, floorUnits, interval = NULL)
 #' @param removeGaps Datenlücken nicht interpolieren/zeichnen
 #' @param plotType Plot-Typ: line oder point
 #' @param plotTitle Überschrift (optional)
-#' @param plotTextPrefix Präfix für alle Texte
-#'                       (bei NA: Kleinere Schrift, wenn LaTeX gewählt)
-#' @param plotSmallPrefix Präfix für alle kleinen Texte
-#'                        (bei NA: Kleinere Schrift, wenn LaTeX gewählt)
 #' @return Der Plot (unsichtbar)
 plotAggregatedResultsOverTime <- function(
     arbitrageResults,
@@ -195,17 +204,14 @@ plotAggregatedResultsOverTime <- function(
     breakpoints = NULL,
     removeGaps = TRUE,
     plotType = "line",
-    plotTitle = NULL,
-    plotTextPrefix = NA,
-    plotSmallPrefix = NA
+    plotTitle = NULL
 ) {
     # Parameter validieren
     stopifnot(
         is.data.table(arbitrageResults), nrow(arbitrageResults) > 0L,
         !is.null(arbitrageResults$Time),
         is.null(latexOutPath) || (is.character(latexOutPath) && length(latexOutPath) == 1L),
-        is.null(breakpoints) || (is.vector(breakpoints) && length(breakpoints) > 0L),
-        length(plotTextPrefix) == 1L, length(plotSmallPrefix) == 1L
+        is.null(breakpoints) || (is.vector(breakpoints) && length(breakpoints) > 0L)
     )
     
     # Ausgabeoptionen
@@ -221,15 +227,8 @@ plotAggregatedResultsOverTime <- function(
     }
     
     # Einige Bezeichnungen und Variablen
-    if (is.na(plotTextPrefix)) {
-        if (is.null(latexOutPath)) {
-            plotTextPrefix <- ""
-            plotSmallPrefix <- ""
-        } else {
-            plotTextPrefix <- "\\footnotesize "
-            plotSmallPrefix <- "\\small "
-        }
-    }
+    plotTextPrefix <- "\\footnotesize "
+    plotSmallPrefix <- "\\small "
     plotXLab <- "Datum"
     plotYLab <- "Arbitrageergebnis"
     
@@ -299,7 +298,7 @@ plotAggregatedResultsOverTime <- function(
         
         # Punkt-Grafik. Besser, wenn Daten viele Lücken aufweisen
         plot <- plot +
-            geom_point(aes(x=Time, y=PriceDifference, color="1"), size=.25)
+            geom_point(aes(x=Time, y=BestResult, color="1"), size=.25)
         
     } else {
         stop(sprintf("Unbekannter Plot-Typ: %s", plotType))
@@ -327,10 +326,7 @@ plotAggregatedResultsOverTime <- function(
         )
     
     if (!is.null(plotTitle)) {
-        plot <- plot + ggtitle(
-            paste0(plotSmallPrefix, plotTitle)#,
-            #subtitle="\\small Median, 25\\,%- und 75\\,%-Quartil der Monatsdaten"
-        )
+        plot <- plot + ggtitle(paste0(plotSmallPrefix, plotTitle))
     }
     
     if (!is.null(latexOutPath)) {
@@ -364,6 +360,7 @@ plotNumResultsOverTime <- function(
     plotXLab <- "Datum"
     plotYLab <- paste0(timeHorizon, " Beobachtungen")
     plotTextPrefix <- "\\footnotesize "
+    plotSmallPrefix <- "\\small "
     maxValue <- max(arbitrageResults$n)
     
     # Achseneigenschaften
@@ -434,7 +431,7 @@ plotNumResultsOverTime <- function(
         )
     
     if (!is.null(plotTitle)) {
-        plot <- plot + ggtitle(paste0("\\small ", plotTitle))
+        plot <- plot + ggtitle(paste0(plotSmallPrefix, plotTitle))
     }
     
     return(plot)
@@ -442,27 +439,188 @@ plotNumResultsOverTime <- function(
 
 
 
+
+#' Informationen über einen Datensatz für eine Gesamtauswertung
+#' (bspw. in Tabellenform) sammeln
+#' 
+#' @param result Eine Instanz der Klasse `TriangularResult`
+#' @return data.table Tabelle mit Gesamtstatistiken
+collectDatasetSummary <- function(result)
+{
+    stopifnot(inherits(result, "TriangularResult"))
+    
+    numRows <- nrow(result$data)
+    intervalLengthHours <- 
+        difftime(
+            last(result$data$Time),
+            first(result$data$Time),
+            units = "hours"
+        ) |>
+        round() |>
+        as.double()
+    
+    return(data.table(
+        exchange = result$Exchange,
+        exchangeName = result$ExchangeName,
+        numRows = numRows,
+        firstResult = first(result$data$Time),
+        lastResult = last(result$data$Time),
+        intervalLengthHours = intervalLengthHours,
+        numRowsPerHour = numRows / intervalLengthHours,
+        numRowsPerDay = numRows / (intervalLengthHours / 24),
+        numRowsLargerThan_A = length(which(result$data$BestResult >= 1.01)),
+        numRowsLargerThan_B = length(which(result$data$BestResult >= 1.02)),
+        numRowsLargerThan_C = length(which(result$data$BestResult >= 1.05)),
+        maxValue = max(result$data$BestResult)
+    ))
+}
+
+
+#' Informationen über einen Datensatz als LaTeX-Tabelle ausgeben
+#' 
+#' @param dataset `data.table`, eine Kollektion aus `collectDatasetSummary`
+#' @param outFile Zieldatei
+#' @param caption Tabellentitel
+#' @param label Tabellenlabel
+summariseDatasetAsTable <- function(
+    dataset,
+    outFile = NULL,
+    caption = NULL,
+    label = NULL
+)
+{
+    printf("Erzeuge Überblickstabelle in %s...\n", basename(outFile))
+    numRowsTotal <- sum(dataset$numRows)
+    
+    # Tabellenzeile erzeugen
+    createRow <- function(dataSubset, end="\\\\\n\n") {
+        s <- strrep(" ", 12) # Einrückung in der Ergebnisdatei
+        return(paste0(
+            sprintf("%s%% %s Datensätze pro Tag\n", s,
+                    format.numberWithFixedDigits(dataSubset$numRowsPerDay, digits=1L)),
+            sprintf("%s\\makecell*[r]{%s\\\\(%s\\,\\%%)} &\n", s, 
+                    format.number(dataSubset$numRows),
+                    format.percentage(dataSubset$numRows / numRowsTotal, 1L)),
+            sprintf("%s%s &\n", s, format.numberWithFixedDigits(dataSubset$numRowsPerHour, 1)),
+            sprintf("%s\\makecell*[r]{%s\\\\(%s\\,\\%%)} &\n", s, 
+                    format.number(dataSubset$numRowsLargerThan_A),
+                    format.percentage(dataSubset$numRowsLargerThan_A / dataSubset$numRows, 1L)),
+            sprintf("%s\\makecell*[r]{%s\\\\(%s\\,\\%%)} &\n", s, 
+                    format.number(dataSubset$numRowsLargerThan_B),
+                    format.percentage(dataSubset$numRowsLargerThan_B / dataSubset$numRows, 1L)),
+            sprintf("%s\\makecell*[r]{%s\\\\(%s\\,\\%%)} &\n", s, 
+                    format.number(dataSubset$numRowsLargerThan_C),
+                    format.percentage(dataSubset$numRowsLargerThan_C / dataSubset$numRows, 1L)),
+            sprintf("%s%s\\,\\%% ", s, 
+                    format.percentage(dataSubset$maxValue, 1)),
+            end
+        ))
+    }
+    
+    # Tabelleninhalt initialisieren
+    tableContent <- ""
+    
+    # Tabelle kann im Fließtext unterkommen
+    tablePosition <- "tbh"
+    
+    # Jede Börse durchgehen
+    for (i in seq_len(nrow(dataset))) {
+        dataSubset <- dataset[i]
+        tableContent <- paste0(
+            tableContent,
+            "\n",
+            sprintf("        %s &\n", dataSubset$exchangeName),
+            createRow(dataSubset)
+        )
+    }
+    
+    # Gesamtdaten berechnen
+    tableContent <- paste0(
+        tableContent,
+        "        \\tablebody\n\n",
+        "        \\rowcolor{white}\n",
+        "        Gesamt &\n",
+        createRow(data.table(
+            numRows = numRowsTotal,
+            numRowsPerHour = numRowsTotal / max(dataset$intervalLengthHours),
+            numRowsPerDay = numRowsTotal / (max(dataset$intervalLengthHours) / 24),
+            numRowsLargerThan_A = sum(dataset$numRowsLargerThan_A),
+            numRowsLargerThan_B = sum(dataset$numRowsLargerThan_B),
+            numRowsLargerThan_C = sum(dataset$numRowsLargerThan_C),
+            maxValue = max(dataset$maxValue)
+        ))
+    )
+    
+    # Tabelle einfach ausgeben
+    if (is.null(outFile)) {
+        printf(tableContent)
+        return(invisible(NULL))
+    }
+    
+    # Tabelle schreiben
+    if (file.exists(outFile)) {
+        Sys.chmod(outFile, mode="0644")
+    }
+    summaryTableTemplateFile |>
+        read_file() |>
+        str_replace(coll("{tablePosition}"), tablePosition) |>
+        str_replace(coll("{tableCaption}"), caption) |>
+        str_replace(coll("{tableContent}"), tableContent) |>
+        str_replace(coll("{tableLabel}"), label) |>
+        write_file(outFile)
+    
+    # Vor versehentlichem Überschreiben schützen
+    Sys.chmod(outFile, mode="0444")
+    
+    return(invisible(NULL))
+}
+
+
+
 # Haupt-Auswertungsfunktion ---------------------------------------------------
 
-analyseTriangularArbitrage <- function(exchange, currency_a, currency_b)
+#' Berechne und analysiere Ergebnisse der Dreiecksarbitrage in grafischer
+#' Form und berechne Daten für eine Auswertung in tabellarischer Form
+#'
+#' @param exchange Die zu betrachtende Börse
+#' @param currency_a Gegenwährung 1
+#' @param currency_b Gegenwährung 2
+#' @param threshold Zeitliche Differenz zweier BTC-Ticks in Sekunden,
+#'                  ab der das Tick-Paar verworfen wird.
+#' @return `result` (invisible) Instanz der Klasse `TriangularResult`
+analyseTriangularArbitrage <- function(
+    exchange,
+    currency_a,
+    currency_b,
+    threshold = 5L
+)
 {
     # Parameter validieren
     stopifnot(
         is.character(exchange), length(exchange) == 1L,
         is.character(currency_a), length(currency_a) == 1L, nchar(currency_a) == 3L,
-        is.character(currency_b), length(currency_b) == 1L, nchar(currency_b) == 3L#,
-        #length(removeOutliers) == 1L, is.logical(removeOutliers), !is.na(removeOutliers)
+        is.character(currency_b), length(currency_b) == 1L, nchar(currency_b) == 3L,
+        is.numeric(threshold), length(threshold) == 1L
     )
     
-    # Variablen initialisieren
-    exchangeName <- exchanges[[exchange]]
+    # Verzeichnisse anlegen
+    plotOutPath <- sprintf(
+        "%s/Abbildungen/Dreiecksarbitrage/%ds/btc-eur-usd/%s",
+        latexOutPath, threshold, exchange
+    )
+    for (d in c(plotOutPath)) {
+        if (!dir.exists(d)) {
+            dir.create(d, recursive=TRUE)
+        }
+    }
     
-    # TODO
-    breakpoints <- NULL
+    # Variablen initialisieren
+    exchangeName <- exchangeNames[[exchange]]
+    breakpoints <- breakpointsByExchange[[exchange]]
     
     dataFile <- sprintf(
-        "Cache/Dreiecksarbitrage 5s/%s-%s-%s-1.fst",
-        exchange, currency_a, currency_b
+        "Cache/Dreiecksarbitrage %ds/%s-%s-%s-1.fst",
+        threshold, exchange, currency_a, currency_b
     )
     stopifnot(file.exists(dataFile))
     
@@ -508,7 +666,7 @@ analyseTriangularArbitrage <- function(exchange, currency_a, currency_b)
     
     # Zusammenfassung ausgeben
     numTotal <- nrow(result$data)
-    printf("%s Datensätze Davon:\n", format.number(numTotal))
+    printf("%s Datensätze. Davon:\n", format.number(numTotal))
     for (largerThan in c(5, 2, 1, .5)) {
         numLarger <- length(result$data[BestResult >= (1 + largerThan / 100), which=TRUE])
         printf(
@@ -520,14 +678,14 @@ analyseTriangularArbitrage <- function(exchange, currency_a, currency_b)
     # Monatsdaten berechnen
     resultsByMonth <- aggregateResultsByTime(result, "1 month")
     
-    # Ergebnisse
+    # Überblicksgrafik
     p_diff <- plotAggregatedResultsOverTime(
         resultsByMonth,
         breakpoints = breakpoints,
         # Lücken werden immer auf 1d-Basis entfernt.
         # Da es sich um Monatsdaten handelt, wäre der Plot leer...
-        removeGaps = FALSE#,
-        #plotTitle = "Arbitrageergebnis"
+        removeGaps = FALSE,
+        plotTitle = "Arbitrageergebnis"
     )
     p_nrow <- plotNumResultsOverTime(
         resultsByMonth,
@@ -535,15 +693,54 @@ analyseTriangularArbitrage <- function(exchange, currency_a, currency_b)
         timeHorizon = "",
         plotTitle = "Anzahl monatlicher Beobachtungen"
     )
+    
+    # Als LaTeX-Dokument ausgeben
+    source("Konfiguration/TikZ.R")
+    tikz(
+        file = sprintf("%s/Uebersicht.tex", plotOutPath),
+        width = documentPageWidth,
+        height = 11 / 2.54,
+        sanitize = TRUE
+    )
+    grid.arrange(
+        p_diff, p_nrow,
+        layout_matrix = rbind(c(1),c(2))
+    )
+    dev.off()
+    
+    return(collectDatasetSummary(result))
 }
 
 
 # Auswertung händisch starten
 if (FALSE) {
     
-    analyseTriangularArbitrage("bitfinex", "usd", "eur")
-    analyseTriangularArbitrage("bitstamp", "usd", "eur")
-    analyseTriangularArbitrage("coinbase", "usd", "eur")
-    analyseTriangularArbitrage("kraken", "usd", "eur")
+    bitfinexSummary <- analyseTriangularArbitrage("bitfinex", "usd", "eur") ; invisible(gc())
+    bitstampSummary <- analyseTriangularArbitrage("bitstamp", "usd", "eur") ; invisible(gc())
+    coinbaseSummary <- analyseTriangularArbitrage("coinbase", "usd", "eur") ; invisible(gc())
+    krakenSummary <- analyseTriangularArbitrage("kraken", "usd", "eur") ; invisible(gc())
+    
+    # Zusammenfassende Tabelle erstellen
+    totalSummary <- rbindlist(list(
+        bitfinexSummary, bitstampSummary, coinbaseSummary, krakenSummary
+    ))
+    # TODO Pfad auf btc-eur-usd korrigieren (Angleich an Plots)
+    tableOutPath <- sprintf(
+        "%s/Tabellen/Dreiecksarbitrage/5s/BTCEURUSD/Uebersicht.tex",
+        latexOutPath
+    )
+    
+    if (!dir.exists(dirname(tableOutPath))) {
+        dir.create(dirname(tableOutPath), recursive=TRUE)
+    }
+    
+    summariseDatasetAsTable(
+        totalSummary,
+        outFile = tableOutPath,
+        caption = sprintf(
+            "Zentrale Kenngrößen der Dreiecksarbitrage für BTC, EUR und USD im Gesamtüberblick"
+        ),
+        label = sprintf("Dreiecksarbitrage_BTCEURUSD_Uebersicht_5s")
+    )
     
 }
