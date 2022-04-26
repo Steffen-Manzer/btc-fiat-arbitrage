f3eca8650006c8b5093f5715df3a08b0e123aee4
fox34 <github@noecho.de>
2022-04-26 11:43:32 +0200

    [Dreiecksarbitrage] Auswertung: Arbitrageergebnis im Zeitverlauf

--- Dreiecksarbitrage/Auswertung nach Börsen gruppiert.R
+++ Dreiecksarbitrage/Auswertung nach Börsen gruppiert.R
@@ -24,20 +24,36 @@ source("Funktionen/printf.R")
 source("Konfiguration/FilePaths.R")
 library("fst")
 library("data.table")
-#library("lubridate") # floor_date
+library("lubridate") # floor_date
 library("ggplot2")
 library("ggthemes")
-library("scales") # breaks_extended
-
+#library("scales") # breaks_extended
+library("tictoc")
 
 
 # Konfiguration -----------------------------------------------------------
+plotAsLaTeX <- FALSE
 exchanges <- list(
     "bitfinex"="Bitfinex",
     "bitstamp"="Bitstamp",
     "coinbase"="Coinbase Pro",
     "kraken"="Kraken"
 )
+
+#' Tabellen-Template mit `{tableContent}`, `{tableCaption` und `{tableLabel}` 
+#' als Platzhalter
+summaryTableTemplateFile <- 
+    sprintf("%s/Tabellen/Templates/Dreiecksarbitrage_Uebersicht_nach_Boerse.tex",
+            latexOutPath)
+
+# Variablen für Tests initialisieren
+exchange <- "coinbase"
+currency_a <- "usd"
+currency_b <- "eur"
+
+# Ergebnisse für Test-/Entwicklungszwecke in globale Umgebung exportieren,
+# um nicht jedes Mal Daten neu einlesen und berechnen zu müssen
+DEBUG_ASSIGN_TO_GLOBAL_ENV <- TRUE
 
 
 # Hilfsfunktionen -------------------------------------------------------------
@@ -102,128 +118,229 @@ calculateResult <- function(result)
 }
 
 
-#' VERALTET: Verteilungsdichte zeichnen
+#' Aggregiere auf den angegebenen Zeitraum
 #' 
-#' @param route Tabelle mit Ergebnissen in der Spalte `Result`
-#' @param removeOutliers Ausreißer entfernen (IQR-Methode)
-#' @param plotTitle Plot-Titel
-#' @param plotSubtitle Plot-Untertitel
-#' @return ggplot-Objekt
-plotDensity <- function(
-        result,
-        removeOutliers = TRUE,
-        plotTitle = NA,
-        plotSubtitle = NA
+#' @param result Eine Instanz der Klasse `TriangularResult`
+#' @param floorUnits Aggregations-Zeitfenster, genutzt als `unit` für
+#'   `floor_date`. Werte kleiner als ein Tag sind grafisch kaum darstellbar.
+#' @param interval Nur dieses Intervall berücksichtigen
+#' @return `data.table` mit Min, Q1, Median, Mean, Q3, Max
+aggregateResultsByTime <- function(result, floorUnits, interval = NULL)
+{
+    # Parameter validieren
+    stopifnot(
+        inherits(result, "TriangularResult"),
+        is.character(floorUnits), length(floorUnits) == 1L,
+        is.null(interval) || length(interval) == 2L
+    )
+    
+    tic()
+    
+    # Gruppierungsfunktion herausgezogen, um ein Kopieren der
+    # großen Datenmengen (für den Fall, dass ein Intervall angegeben ist)
+    # unten zu vermeiden (stattdessen immer direkt ein data.table-Subsetting)
+    group <- expr(.(
+        Min = min(BestResult),
+        Q1 = quantile(BestResult, probs=.25, names=FALSE),
+        Mean = mean(BestResult),
+        Median = median(BestResult),
+        Q3 = quantile(BestResult, probs=.75, names=FALSE),
+        Max = max(BestResult),
+        n = .N
+    ))
+    
+    if (!is.null(interval)) {
+        
+        # Zeitraum begrenzt
+        # TODO Test
+        aggregatedResults <- result$data[
+            Time %between% interval,
+            j = eval(group),
+            by = .(Time=floor_date(Time, unit=floorUnits))
+        ]
+        
+    } else {
+        
+        # Gesamte Daten
+        aggregatedResults <- result$data[
+            j = eval(group),
+            by = .(Time=floor_date(Time, unit=floorUnits))
+        ]
+    }
+    
+    printf("Aggregation auf '%s' ergab %s Datensätze. ",
+           floorUnits, format.number(nrow(aggregatedResults)))
+    toc()
+    
+    return(aggregatedResults)
+}
+
+
+#' Zeichne Arbitrageergebnisse als Linien-/Punktgrafik im Zeitverlauf
+#' 
+#' @param arbitrageResults `data.table` mit den aggr. Preisen der verschiedenen Börsen
+#' @param latexOutPath Ausgabepfad als LaTeX-Datei
+#' @param breakpoints Vektor mit Daten (Plural von: Datum) der Strukturbrüche
+#' @param removeGaps Datenlücken nicht interpolieren/zeichnen
+#' @param plotType Plot-Typ: line oder point
+#' @param plotTitle Überschrift (optional)
+#' @param plotTextPrefix Präfix für alle Texte
+#'                       (bei NA: Kleinere Schrift, wenn LaTeX gewählt)
+#' @param plotSmallPrefix Präfix für alle kleinen Texte
+#'                        (bei NA: Kleinere Schrift, wenn LaTeX gewählt)
+#' @return Der Plot (unsichtbar)
+plotAggregatedResultsOverTime <- function(
+    arbitrageResults,
+    latexOutPath = NULL,
+    breakpoints = NULL,
+    removeGaps = TRUE,
+    plotType = "line",
+    plotTitle = NULL,
+    plotTextPrefix = NA,
+    plotSmallPrefix = NA
 ) {
     # Parameter validieren
     stopifnot(
-        is.data.table(result), !is.null(result$Result),
-        length(removeOutliers) == 1L, is.logical(removeOutliers),
-        (is.na(plotTitle) || (length(plotTitle) == 1L && is.character(plotTitle))),
-        (is.na(plotSubtitle) || (length(plotSubtitle) == 1L && is.character(plotSubtitle)))
+        is.data.table(arbitrageResults), nrow(arbitrageResults) > 0L,
+        !is.null(arbitrageResults$Time),
+        is.null(latexOutPath) || (is.character(latexOutPath) && length(latexOutPath) == 1L),
+        is.null(breakpoints) || (is.vector(breakpoints) && length(breakpoints) > 0L),
+        length(plotTextPrefix) == 1L, length(plotSmallPrefix) == 1L
     )
     
-    # Dichteplot ist im Grunde nur ohne Ausreißer interessant
-    # TODO Ausreißer-Definition in Arbeit
-    # Zitat aus Wikipedia:
-    #' Die „Erwartung“ wird meistens als Streuungsbereich um
-    #' den Erwartungswert herum definiert, in dem die meisten aller
-    #' Messwerte zu liegen kommen, z. B. der Quartilabstand Q75 – Q25.
-    #' Werte, die weiter als das 1,5-Fache des Quartilabstandes außerhalb
-    #' dieses Intervalls liegen, werden (*meist willkürlich*) als Ausreißer bezeichnet.
-    s <- summary(result$Result)
-    if (removeOutliers) {
-        iqr <- s[[5]] - s[[2]]
-        lower <- max(s[[1]], s[[2]] - 1.5*iqr)
-        upper <- min(s[[6]], s[[5]] + 1.5*iqr)
-        xlim <- c(lower, upper)
+    # Ausgabeoptionen
+    if (!is.null(latexOutPath)) {
+        source("Konfiguration/TikZ.R")
+        printf.debug("Ausgabe als LaTeX in Datei %s\n", basename(latexOutPath))
+        tikz(
+            file = latexOutPath,
+            width = documentPageWidth,
+            height = 6 / 2.54, # cm -> Zoll
+            sanitize = TRUE
+        )
+    }
+    
+    # Einige Bezeichnungen und Variablen
+    if (is.na(plotTextPrefix)) {
+        if (is.null(latexOutPath)) {
+            plotTextPrefix <- ""
+            plotSmallPrefix <- ""
+        } else {
+            plotTextPrefix <- "\\footnotesize "
+            plotSmallPrefix <- "\\small "
+        }
+    }
+    plotXLab <- "Datum"
+    plotYLab <- "Arbitrageergebnis"
+    
+    # Zeichnen
+    if (!is.null(arbitrageResults$Q3)) {
+        minValue <- min(arbitrageResults$Q1)
+        maxValue <- max(arbitrageResults$Q3)
+    } else if (!is.null(arbitrageResults$BestResult)) {
+        minValue <- min(arbitrageResults$BestResult)
+        maxValue <- max(arbitrageResults$BestResult)
+    } else {
+        stop("Keine Quartile und keine Rohdaten gefunden!")
+    }
+    
+    plot <- ggplot(arbitrageResults)
+    
+    # Bereiche zeichnen und Nummer anzeigen
+    if (!is.null(breakpoints)) {
         
-        # n muss hier hoch sein, da Ausreißer ausgeblendet, nicht
-        # jedoch aus der Berechnung entfernt werden
-        # Sonst wäre die "reingezoomte" Dichte sehr grob
-        #nDensity <- 2^14
-        nDensity <- 2^16 # Bei mehreren Börsen
+        # Die hier bestimmten Intervalle der aggregierten Daten können
+        # von den Intervallen des gesamten Datensatzes abweichen
+        intervals <- calculateIntervals(arbitrageResults$Time, breakpoints)
         
-        # TODO Parameter from, to, cut in `density()`?!
+        # Grafik um farbige Hintergründe der jeweiligen Segmente ergänzen
+        plot <- plot + 
+            geom_rect(
+                aes(
+                    xmin = From,
+                    xmax = To,
+                    ymin = 0,
+                    ymax = maxValue * 1.05,
+                    fill = Set
+                ),
+                data = intervals,
+                alpha = .25
+            ) +
+            geom_text(
+                aes(
+                    x = From+(To-From)/2,
+                    y = maxValue,
+                    label = paste0(plotTextPrefix, Set)
+                ),
+                data = intervals
+            )
+    }
+    
+    if (plotType == "line") {
+        
+        # Liniengrafik. Nützlich, wenn Daten nahezu kontinuierlich vorliegen
+        # Bestehende Lücken > 2 Tage dennoch auslassen und nicht interpolieren
+        # https://stackoverflow.com/a/21529560
+        # TODO Variabel gestalten (nicht fix 2 Tage)?
+        if (removeGaps) {
+            gapGroups <- c(0, cumsum(diff(arbitrageResults$Time) > 2))
+        } else {
+            gapGroups <- 0
+        }
+        
+        plot <- plot +
+            # Q1/Q3 zeichnen
+            geom_ribbon(aes(x=Time, ymin=Q1, ymax=Q3, group=gapGroups), fill="grey70") +
+            
+            # Median zeichnen
+            geom_line(aes(x=Time, y=Median, color="1", linetype="1", group=gapGroups), size=.5)
+        
+    } else if (plotType == "point") {
+        
+        # Punkt-Grafik. Besser, wenn Daten viele Lücken aufweisen
+        plot <- plot +
+            geom_point(aes(x=Time, y=PriceDifference, color="1"), size=.25)
         
     } else {
-        xlim <- c(NA, NA)
-        nDensity <- 2^9
+        stop(sprintf("Unbekannter Plot-Typ: %s", plotType))
     }
     
-    p <- ggplot() +
-        # stat_density mit geom="line" statt geom_density für eine bessere Legende
-        # Siehe https://stackoverflow.com/a/17506172
-        stat_density(
-            aes(x=Result, colour=Exchange, group=Exchange, linetype=Exchange),
-            data = result,
-            geom = "line",
-            position = "identity",
-            n = nDensity
-        ) +
+    
+    plot <- plot + 
         theme_minimal() +
-        scale_x_continuous(
-            labels = function(x) paste0(format.percentage(x - 1, 2L), " %"),
-            breaks = breaks_extended(8)
+        theme(
+            legend.position = "none",
+            plot.title.position = "plot",
+            axis.title.x = element_text(margin=margin(t=5, r=0, b=0, l=0)),
+            axis.title.y = element_text(margin=margin(t=0, r=10, b=0, l=0))
         ) +
-        scale_y_continuous(labels = format.number) +
-        coord_cartesian(xlim=xlim) +
+        scale_x_datetime(expand=expansion(mult=c(.01, .03))) +
+        coord_cartesian(ylim=c(minValue, maxValue)) +
+        scale_y_continuous(
+            labels = function(x) paste(format.number((x-1) * 100), "%")
+        ) +
         scale_color_ptol() +
         scale_fill_ptol() +
-        # TODO y-Achse korrekt: Dichte (density) oder Häufigkeit (in aes: y=after_stat(count))
         labs(
-            x="Ergebnis", y="Dichte", 
-            colour="Börse", linetype="Börse", 
-            title=plotTitle, subtitle=plotSubtitle
+            x = paste0(plotTextPrefix, plotXLab),
+            y = paste0(plotTextPrefix, plotYLab)
         )
-    return(p)
-}
-
-
-#' Groben Überblick in Konsole anzeigen
-#' 
-#' Gibt die Anzahl Datensätze insgesamt und > 1% / 0,5% / 0,2% / 0,1% an
-#' 
-#' @param result Eine Instanz der Klasse `TriangularResult`
-showSummaryStatistics <- function(result)
-{
-    stopifnot(inherits(result, "TriangularResult"))
     
-    numTotal <- nrow(result$data)
-    printf("%s Datensätze, davon:\n", format.number(numTotal))
-    for (largerThan in c(1, .5, .2, .1)) {
-        numLarger <- length(result$data[BestResult >= (1 + largerThan / 100), which=TRUE])
-        printf(
-            "Ergebnis >= %.1f %%: %s (%s %%)\n",
-            largerThan, format.number(numLarger), format.percentage(numLarger/numTotal)
+    if (!is.null(plotTitle)) {
+        plot <- plot + ggtitle(
+            paste0(plotSmallPrefix, plotTitle)#,
+            #subtitle="\\small Median, 25\\,%- und 75\\,%-Quartil der Monatsdaten"
         )
     }
-}
-
-
-#' Zusammenfassende Statistiken und Lagemaße berechnen
-#' 
-#' @param result Eine Instanz der Klasse `TriangularResult`
-#' @return Ergebnisliste
-calculateSummaryStatistics <- function(result)
-{
-    stopifnot(inherits(result, "TriangularResult"))
     
-    return(list(
-        exchange = result$ExchangeName,
-        numRows = nrow(result$data),
-        Max = max(result$data$BestResult),
-        numRowsGt5 = length(result$data[BestResult >= (1 + 5/100), which=TRUE]),
-        numRowsGt2 = length(result$data[BestResult >= (1 + 2/100), which=TRUE]),
-        numRowsGt1 = length(result$data[BestResult >= (1 + 1/100), which=TRUE]),
-        numRowsGtDot5 = length(result$data[BestResult >= (1 + .5/100), which=TRUE]),
-        numRowsGtDot2 = length(result$data[BestResult >= (1 + .2/100), which=TRUE]),
-        numRowsGtDot1 = length(result$data[BestResult >= (1 + .1/100), which=TRUE]),
-        Q1 = quantile(result$data$BestResult, probs=.25, names=FALSE),
-        Mean = mean(result$data$BestResult),
-        Median = median(result$data$BestResult),
-        Q3 = quantile(result$data$BestResult, probs=.75, names=FALSE)
-    ))
+    if (!is.null(latexOutPath)) {
+        # Plot zeichnen
+        print(plot)
+        dev.off()
+        return(invisible(plot))
+    } else {
+        return(plot)
+    }
 }
 
 
@@ -249,35 +366,70 @@ analyseTriangularArbitrage <- function(exchange, currency_a, currency_b)
     stopifnot(file.exists(dataFile))
     
     # Daten einlesen
-    printf("Verarbeite %s...\n", exchangeName)
-    result <- new(
-        "TriangularResult",
-        Exchange = exchange,
-        ExchangeName = exchangeName,
-        Currency_A = currency_a,
-        Currency_B = currency_b,
-        data = read_fst(
-            dataFile,
-            columns = c(
-                "Time",
-                "a_PriceLow", "a_PriceHigh", # z.B. BTC/USD
-                "b_PriceLow", "b_PriceHigh", # z.B. BTC/EUR
-                "ab_Bid", "ab_Ask" # z.B. EUR/USD
-            ),
-            as.data.table = TRUE
+    if (
+        exists("DEBUG_ASSIGN_TO_GLOBAL_ENV") && isTRUE(DEBUG_ASSIGN_TO_GLOBAL_ENV) &&
+        exists("result") && inherits(result, "TriangularResult") &&
+        result$Exchange == exchange
+    ) {
+        # Entwicklungsmodus aktiv: Daten bereits geladen
+        printf("Nutze bereits eingelesene Daten für %s.\n", exchangeName)
+        
+    } else {
+        
+        # Daten neu einlesen
+        printf("Lese Daten für %s...\n", exchangeName)
+        result <- new(
+            "TriangularResult",
+            Exchange = exchange,
+            ExchangeName = exchangeName,
+            Currency_A = currency_a,
+            Currency_B = currency_b,
+            data = read_fst(
+                dataFile,
+                columns = c(
+                    "Time",
+                    "a_PriceLow", "a_PriceHigh", # z.B. BTC/USD
+                    "b_PriceLow", "b_PriceHigh", # z.B. BTC/EUR
+                    "ab_Bid", "ab_Ask" # z.B. EUR/USD
+                ),
+                as.data.table = TRUE
+            )
         )
+        
+        # Ergebnis der Arbitrage (beide Routen + Optimum) berechnen
+        calculateResult(result)
+        
+        # Entwicklungsmodus aktiv, Werte für weitere Entwicklung beibehalten
+        if (exists("DEBUG_ASSIGN_TO_GLOBAL_ENV") && isTRUE(DEBUG_ASSIGN_TO_GLOBAL_ENV)) {
+            result <<- result
+        }
+    }
+    
+    # Zusammenfassung ausgeben
+    numTotal <- nrow(result$data)
+    printf("%s Datensätze Davon:\n", format.number(numTotal))
+    for (largerThan in c(5, 2, 1, .5, .2, .1)) {
+        numLarger <- length(result$data[BestResult >= (1 + largerThan / 100), which=TRUE])
+        printf(
+            "Anzahl >= %.1f %%: %s (%s %%)\n",
+            largerThan, format.number(numLarger), format.percentage(numLarger/numTotal, 1L)
+        )
+    }
+    
+    # Monatsdaten berechnen
+    resultsByMonth <- aggregateResultsByTime(result, "1 month")
+    
+    # Ergebnisse
+    p_diff <- plotAggregatedResultsOverTime(
+        resultsByMonth,
+        #breakpoints = breakpoints,
+        # Lücken werden immer auf 1d-Basis entfernt.
+        # Da es sich um Monatsdaten handelt, wäre der Plot leer...
+        removeGaps = FALSE#,
+        #plotTitle = "Arbitrageergebnis"
     )
-    
-    # Ergebnis der Arbitrage (beide Routen + Optimum) berechnen
-    calculateResult(result)
-    
-    # Hier weiter...
 }
 
-# Bootstrap für Tests
-exchange <- "coinbase"
-currency_a <- "usd"
-currency_b <- "eur"
 
 # Auswertung händisch starten
 if (FALSE) {
